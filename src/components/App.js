f31473ef20a3c4e8181c39725a95e1e5e54a1bc0
Seth Campbell <sethrcamp@gmail.com>
2017-07-18 21:00:43 -0400

    services route

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Footer from './Footer';
 import Home from './Home/Home';
 import Bio from './Bio/Bio';
 import Gallery from './Gallery/Gallery';
+import Services from './Services/Services';
 
 export default class App extends React.Component {
     render() {
@@ -16,7 +17,7 @@ export default class App extends React.Component {
                     <Route exact path="/" component={Home}/>
                     <Route path="/bio/" component={Bio}/>
                     <Route path="/gallery/" component={Gallery}/>
-                    <Route/>
+                    <Route path="/services/" component={Services}/>
                     <Route/>
                 </Switch>
                 <Footer/>
