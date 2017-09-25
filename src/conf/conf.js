9f9696b6a4063220aadee672501316a2fc22a9da
Seth Campbell <sethrcamp@gmail.com>
2017-09-25 12:38:17 -0400

    changed the base url to resemble a symbolic link on the server

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -2,4 +2,4 @@
 export const BASE_PATH = "https://s3.us-east-2.amazonaws.com/dreamesquephotography/";
 // export const BASE_PATH = "192.168.1.84:8888/dream_frontend/src/assets/";
 // export const BASE_URL = "http://localhost:8888/dream_backend/public/";
-export const BASE_URL = "http://dreamesquephotography.com/dreamesque_home/dream_backend/public/";
+export const BASE_URL = "http://dreamesquephotography.com/backend_link/public/";
